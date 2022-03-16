d38d89405f1b28e35d1ec7fe005f97da8c41621c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-16 19:35:20 -0400

    checks actually working now

--- src/components/ActionItem.jsx
+++ src/components/ActionItem.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 const ActionItem = (props) => {
-    const [a, setA] = useState("START")
+  const [a, setA] = useState("START");
 
-
-const checkBoxHandler = (event) => {
-    setA("END")
-}
+  const checkBoxHandler = (event) => {
+    if (a == "START") {
+      setA("END");
+    } else {
+      setA("START");
+    }
+  };
 
   return (
     <form id="row">
-        <h1>{a}</h1>
+      <h1>{a}</h1>
       <input type="text" id="textbox" value={props.text} />
-      <input type="checkbox" className="form-check-input" onChange={checkBoxHandler}/>
+      <input
+        type="checkbox"
+        className="form-check-input"
+        onChange={checkBoxHandler}
+      />
       <h1>{props.check ? "X" : "O"}</h1>
       <button className="btn btn-primary mx-4" type="submit">
         Delete
